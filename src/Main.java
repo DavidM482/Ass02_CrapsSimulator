d90d4b84ba812683eecf95cfe85d34b3e15eedf3
DavidM482 <martin.613901@swl.k12.oh.us>
2024-11-11 08:55:20 -0500

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.Math;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        //variables
+        String playAgainYN = "";
+        boolean valid = false;
+        int sum = 0;
+        int roll1 = 0;
+        int roll2 = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Welcome to the Craps simulator!");
+        System.out.println("Rule 1: If the sum is 2, 3, or 12 it is called 'crapping out' and the game is over with a loss. ");
+        System.out.println("Rule 2: If the sum is 7 or 11 it is called a 'natural' and the game is over with a win.");
+        System.out.println("Rule 3: For all other values, the sum becomes 'the point' and the user makes subsequent rolls until");
+        System.out.println("they either roll a 7, in which case they lose, or they roll the point sum in which case they win.");
+
+        do {
+            roll1 = (int) (Math.random() * 6) + 1;
+            roll2 = (int) (Math.random() * 6) + 1;
+            sum = roll1 + roll2;
+            System.out.printf("\n%-7s %2d", "roll 1:", roll1);
+            System.out.printf("\n%-7s %2d", "roll 2:", roll2);
+            System.out.printf("\n%-7s %2d\n", "sum:", sum);
+
+            if (sum == 2 || sum == 3 || sum == 12) {
+                System.out.println("Sorry, you crapped out.");
+                valid = true;
+            } else if (sum == 7 || sum == 11) {
+                System.out.println("You Win!");
+                valid = true;
+            } else {
+                System.out.println("Reroll. You must roll a " + sum + " in order to win. A roll of 7 will result in a loss.");
+            }
+            while (!valid) {
+                roll1 = (int) (Math.random() * 6) + 1;
+                roll2 = (int) (Math.random() * 6) + 1;
+                System.out.printf("\n%-7s %2d", "Roll 1: ", roll1);
+                System.out.printf("\n%-7s %2d", "Roll 2: ",roll2);
+                System.out.printf("\n%-7s %2d\n", "Sum:", roll1 + roll2);
+                if (roll1 + roll2 == sum) {
+                    System.out.println("You win!");
+                    valid = true;
+                } else if (roll1 + roll2 == 7) {
+                    System.out.println("Sorry, you crapped out.");
+                    valid = true;
+                } else {
+                    System.out.println("Reroll. You must roll a " + sum + " in order to win. A roll of 7 will result in a loss.");
+                }
+            }
+
+            System.out.println("\n\nPlay Again? [Y/N]");
+            playAgainYN = scan.nextLine();
+            valid = false;
+        } while (playAgainYN.equalsIgnoreCase("y") || playAgainYN.equalsIgnoreCase("yes"));
     }
 }
